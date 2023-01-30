7e937d3da205ed2f82d871df65939846e317684f
Kaung Htun Thant <3199busi@gmail.com>
2023-01-30 15:14:11 +0630

    Added Employee Controller

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function show()
+    {
+        
+    }
+    public function update()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +28,35 @@ Route::post('/login/submit', [TestController::class, 'login']);
 
 Route::get('/dashboard', [TestController::class, 'index']);
 
+Route::resource('/dashboard/employees', EmployeeController::class);
 //Private Routes
-Route::group(['prefix'=>'accounts','as'=>'account.'], function(){
-
-});
+// Route::group(['prefix'=>'/dashboard'], function(){
+//     Route::resource('employees', EmployeeController::class);
+//     Route::resource('students', Controller::class);
+//     Route::resource('employees', Controller::class);
+//     Route::resource('employees', Controller::class);
+//     Route::resource('employees', Controller::class);
+//     Route::resource('employees', Controller::class);
+//     Route::resource('employees', Controller::class);
+//     Route::group(['prefix'=>'/dashboard/students'], function(){
+    
+//     });
+//     Route::group(['prefix'=>'/dashboard/users'], function(){
+    
+//     });
+//     Route::group(['prefix'=>'/dashboard/campus'], function(){
+    
+//     });
+//     Route::group(['prefix'=>'/dashboard/departments'], function(){
+    
+//     });
+//     Route::group(['prefix'=>'/dashboard/positions'], function(){
+    
+//     });
+//     Route::group(['prefix'=>'/dashboard/classes'], function(){
+    
+//     });
+//     Route::group(['prefix'=>'/dashboard/batches'], function(){
+    
+//     });
+// });
