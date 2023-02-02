b3b7f39b7de8777cd98a3331d30c419933420ea7
Kaung Htun Thant <3199busi@gmail.com>
2023-02-02 11:15:49 +0630

    Image changed

--- resources/views/empqrview.blade.php
+++ resources/views/empqrview.blade.php
@@ -16,7 +16,7 @@
     <table border="1">
         @foreach ($contents as $content)
         <tr>
-            <th colspan="2">QR Code<br><img src="https://idserver.kbtc.edu.mm/qrcodes/{{ $content->empCardID }}.png"></th>
+            <th colspan="2">QR Code<br><img src="https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg"></th>
         </tr>
         <tr>
             <td>
