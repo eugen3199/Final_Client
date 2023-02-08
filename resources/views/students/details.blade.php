9f29035599e8385c78825c83f5ac30d1a022bc52
Kaung Htun Thant <3199busi@gmail.com>
2023-02-08 13:42:09 +0630

    student client bug fix again2

--- resources/views/students/details.blade.php
+++ resources/views/students/details.blade.php
@@ -6,7 +6,6 @@
     <table>
         <tr>
             <th colspan="2">{{ $student->studName }}'s Profile<br><img src="https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg" width="200px">
-                {{ route('students.update', $student->id) }}
             </th>
         </tr>
         <tr>
